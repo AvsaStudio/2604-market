9fe15225c625822a6f6d16a159c41eeefd8e74c9
AvsaStudio <salome.a.miller@gmail.com>
2026-06-10 21:48:13 -0400

    Seed the database with at least 1 user who has made at least 1 order of at least 5 distinct products.

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,25 @@
 import db from "#db/client";
 
+import { createOrder } from "#db/queries/orders";
+import { createOrderProduct } from "#db/queries/orders_products";
+import { createProduct } from "#db/queries/products";
+import { createUser } from "#db/queries/users";
+
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  for (let i = 1; i <= 10; i++) {
+    await createProduct("Product " + i, "Description " + i, i * 10);
+  }
+
+  const user = await createUser("user", "password");
+
+  const order = await createOrder("2026-06-10", "order", user.id);
+
+  for (let i = 1; i <= 5; i++) {
+    await createOrderProduct(order.id, i, i);
+  }
 }
